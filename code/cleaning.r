db0a61a44ab22e4b701269419bc0f14a1b132dac
Gustavo Useche <gustavo@usecheb.com>
2025-04-08 23:04:52 +0200

    Initial version of hosp_dataframe, translation to spanish

--- code/cleaning.r
+++ code/cleaning.r
@@ -63,12 +63,12 @@ demogr_df <- main_df |>
     period == "p2" ~ "2024"),
     age = as.numeric(difftime(status_date, birth_date, units = "days")) / 365.25)
 
-# Codigo para generar la tabla comparativa demográfica
+# Código para generar la tabla comparativa demográfica
 demogr_tb <- demogr_df |> 
   tbl_summary(
     by = period,
     include = c(age, gender, htn, diabetes, status, charlson, karnofsky, va),
-    label = list( # TODO: traducir las etiquetas al español
+    label = list(
       age ~ "Edad",
       gender ~ "Mujeres",
       htn ~ "Hipertensión",
@@ -85,4 +85,102 @@ demogr_tb <- demogr_df |>
   add_p(test.args = c(karnofsky, status) ~ list(simulate.p.value = TRUE))
 
 print(demogr_tb)
+
+
+# Generar dataframe para extraer análisis de hospitalizaciones ####
+# Primero convertir a formato largo, para generar nueva columna con la información de los periodos
+
+hosp_df <- main_df |> 
+  select(id, contains("hosp")) |> 
+  pivot_longer(
+    cols = matches("_[1-9]"),
+    names_to = c(".value", "period"),
+    names_pattern = "(.+?)_([1-9])") |> 
+  filter(!is.na(hosp_start)) |>   # Recoge todos los eventos hospitalarios en ambos periodos
+  mutate(
+    hosp_length = time_length(interval(hosp_start, hosp_end), "days"),
+    hosp_cause_groups = factor(case_match(
+      hosp_cause,
+      "Infection Respiratory" ~ "Infección",
+      "Cardiovascular Pulmonary Disease" ~ "Cardiovascular",
+      "Digestive System Disorder" ~ "Otras Causas",
+      "Other" ~ "Otras Causas",
+      "Access Repair" ~ "Acceso Vascular (no infección)",
+      "Musculoskeletal System Disorder" ~ "Otras Causas",
+      "Infection Blood" ~ "Infección",
+      "Cardiovascular Vessel Disease" ~ "Cardiovascular",
+      "Genitourinary Disorder" ~ "Urológicas",
+      "Cardiovascular Heart Disease" ~ "Cardiovascular",
+      "Access Infection" ~ "Infección",
+      "Immune Disorder Other" ~ "Otras Causas",
+      "Unknown" ~ "Desconocido",
+      "Infection Other" ~ "Infección",
+      "Nervous System Disorder" ~ "Otras Causas",
+      "Access Other" ~ "Acceso Vascular (no infección)",
+      "Circulatory Disease" ~ "Cardiovascular",
+      "Infection Wound" ~ "Infección",
+      "Respiratory System Other Cause" ~ "Otras Causas",
+      "Infection COVID19 (SARS-CoV2 infection)" ~ "Infección",
+      "Access Placement" ~ "Acceso Vascular (no infección)",
+      "Blood Disorder Anemia" ~ "Otras Causas",
+      "Cerebrovascular Disease" ~ "Cardiovascular",
+      "Injury/Accident" ~ "Otras Causas",
+      "Cardiovascular Other" ~ "Cardiovascular",
+      "Blood Disorder Clotting" ~ "Otras Causas",
+      "Blood Disorder Other" ~ "Otras Causas",
+      "Endocrine Disorder Diabetes" ~ "Otras Causas",
+      "Cancer/Neoplasms" ~ "Otras Causas",
+      "Metabolic Disorder" ~ "Otras Causas",
+      "Respiratory System COPD" ~ "Otras Causas",
+      "Mental Disorder" ~ "Otras Causas",
+      "Infection Peritonitis (PD Related Peritonitis Excluded)" ~ "Infección",
+      "Skin Disorder" ~ "Otras Causas",
+      "Infection Tunnel/Cuff" ~ "Infección",
+      "PD Catheter Replacement" ~ "Otras Causas",
+      "Other PD Related Cause" ~ "Otras Causas",
+      "PD Catheter Placement" ~ "Otras Causas")),
+    hosp_cause = case_match(
+      hosp_cause,
+      "Infection Respiratory" ~ "Infección Respiratoria",
+      "Cardiovascular Pulmonary Disease" ~ "Enfermedad Pulmonar Cardiovascular",
+      "Digestive System Disorder" ~ "Trastorno del Sistema Digestivo",
+      "Other" ~ "Otras Causas",
+      "Access Repair" ~ "Reparación de Acceso Vascular",
+      "Musculoskeletal System Disorder" ~ "Trastorno del Sistema Musculoesquelético",
+      "Infection Blood" ~ "Bacteriemia",
+      "Cardiovascular Vessel Disease" ~ "Enfermedad Cardiovascular Periférica",
+      "Genitourinary Disorder" ~ "Trastorno Genitourinario",
+      "Cardiovascular Heart Disease" ~ "Enfermedad Cardiovascular",
+      "Access Infection" ~ "Infección de Acceso Vascular",
+      "Immune Disorder Other" ~ "Otros Trastornos Inmunológicos",
+      "Unknown" ~ "Desconocido",
+      "Infection Other" ~ "Otras Infecciones",
+      "Nervous System Disorder" ~ "Trastorno del Sistema Nervioso",
+      "Access Other" ~ "Otras Causas - Acceso Vascular",
+      "Circulatory Disease" ~ "Enfermedad Circulatoria",
+      "Infection Wound" ~ "Infección de Herida",
+      "Respiratory System Other Cause" ~ "Otras Causas Respiratorias",
+      "Infection COVID19 (SARS-CoV2 infection)" ~ "Infección COVID19 (Infección por SARS-CoV2)",
+      "Access Placement" ~ "Colocación de Acceso Vascular",
+      "Blood Disorder Anemia" ~ "Anemia",
+      "Cerebrovascular Disease" ~ "Enfermedad Cerebrovascular",
+      "Injury/Accident" ~ "Lesión/Accidente",
+      "Cardiovascular Other" ~ "Otros Trastornos Cardiovasculares",
+      "Blood Disorder Clotting" ~ "Trastorno de la Coagulación",
+      "Blood Disorder Other" ~ "Otros Trastornos Hematológicos",
+      "Endocrine Disorder Diabetes" ~ "Diabetes",
+      "Cancer/Neoplasms" ~ "Cáncer/Neoplasmas",
+      "Metabolic Disorder" ~ "Trastorno Metabólico",
+      "Respiratory System COPD" ~ "Enfermedad Pulmonar Obstructiva Crónica (EPOC)",
+      "Mental Disorder" ~ "Trastorno Mental",
+      "Infection Peritonitis (PD Related Peritonitis Excluded)" ~ "Peritonitis (Excluyendo Peritonitis Relacionada con PD)",
+      "Skin Disorder" ~ "Trastorno de la Piel",
+      "Infection Tunnel/Cuff" ~ "Infección de Túnel/Cuff",
+      "PD Catheter Replacement" ~ "Reemplazo de Catéter PD",
+      "Other PD Related Cause" ~ "Otras Causas Relacionadas con PD",
+      "PD Catheter Placement" ~ "Colocación de Catéter PD"))
+
+
+# Ánalisis de hospitalizaciones ####
+
 
