4250f3c9d2c1c9c4801cfda859c6577e7d9856a8
Gustavo Useche <gustavo@usecheb.com>
2025-04-09 21:51:20 +0200

    Add ggplot to required packages and only leave cleaning code

--- code/cleaning.r
+++ code/cleaning.r
@@ -2,6 +2,7 @@
 require(tidyverse)
 require(lubridate)
 require(gtsummary)
+library(ggplot2)
 
 # Definir locale, cargar dataframe y renombrar columnas ####
 
@@ -48,139 +49,13 @@ main_df <- read_csv("./data/raw/dana_24_25.csv",
                       labels = c("No", "Si")))
 
 
-# Generar dataframe para extraer análisis demográfico ####
-# Primero convertir a formato largo, para generar nueva columna con la información del periodo
-# y poder usar ''gtsummary'' para la tabla comparativa de poblaciones
-demogr_df <- main_df |> 
-  select(id, gender, birth_date, htn, diabetes, diabetes_type, contains("p1"), contains("p2")) |> 
-  pivot_longer(
-    cols = matches("_p[12]"),
-    names_to = c(".value", "period"),
-    names_pattern = "(.+?)_(p[12])") |> 
-  filter(!is.na(clinic) & !is.na(va)) |>           # Este filter permite eliminar entradas duplicadas de pacientes que solo
-  mutate(period = case_when(                       # están presentes un periodo y aquellos que iniciaron después del periodo
-    period == "p1" ~ "2023",
-    period == "p2" ~ "2024"),
-    age = as.numeric(difftime(status_date, birth_date, units = "days")) / 365.25)
+# Definir los periodos de análisis
 
-# Código para generar la tabla comparativa demográfica
-demogr_tb <- demogr_df |> 
-  tbl_summary(
-    by = period,
-    include = c(age, gender, htn, diabetes, status, charlson, karnofsky, va),
-    label = list(
-      age ~ "Edad",
-      gender ~ "Mujeres",
-      htn ~ "Hipertensión",
-      diabetes ~ "Diabetes", 
-      charlson ~ "Índice de Charlson",
-      karnofsky ~ "Karnofsky",  # TODO: ¿mostrar entero o agruparlo?
-      va ~ "Acceso Vascular"),
-    statistic = list(
-      age ~ "{mean} (\u00B1{sd})"),
-    value = list(
-      gender ~ "Mujer",
-      c(htn, diabetes) ~ "Si")
-  ) |> 
-  add_p(test.args = c(karnofsky, status) ~ list(simulate.p.value = TRUE))
+period_predana_start <- date("2023-10-28")
+period_predana_end <- date("2024-02-28")
 
-print(demogr_tb)
+period_postdana_start <- date("2024-10-28")
+period_postdana_end <- date("2025-02-28")
 
-
-# Generar dataframe para extraer análisis de hospitalizaciones ####
-# Primero convertir a formato largo, para generar nueva columna con la información de los periodos
-
-hosp_df <- main_df |> 
-  select(id, contains("hosp")) |> 
-  pivot_longer(
-    cols = matches("_[1-9]"),
-    names_to = c(".value", "period"),
-    names_pattern = "(.+?)_([1-9])") |> 
-  filter(!is.na(hosp_start)) |>   # Recoge todos los eventos hospitalarios en ambos periodos
-  mutate(
-    hosp_length = time_length(interval(hosp_start, hosp_end), "days"),
-    hosp_cause_groups = factor(case_match(
-      hosp_cause,
-      "Infection Respiratory" ~ "Infección",
-      "Cardiovascular Pulmonary Disease" ~ "Cardiovascular",
-      "Digestive System Disorder" ~ "Otras Causas",
-      "Other" ~ "Otras Causas",
-      "Access Repair" ~ "Acceso Vascular (no infección)",
-      "Musculoskeletal System Disorder" ~ "Otras Causas",
-      "Infection Blood" ~ "Infección",
-      "Cardiovascular Vessel Disease" ~ "Cardiovascular",
-      "Genitourinary Disorder" ~ "Urológicas",
-      "Cardiovascular Heart Disease" ~ "Cardiovascular",
-      "Access Infection" ~ "Infección",
-      "Immune Disorder Other" ~ "Otras Causas",
-      "Unknown" ~ "Desconocido",
-      "Infection Other" ~ "Infección",
-      "Nervous System Disorder" ~ "Otras Causas",
-      "Access Other" ~ "Acceso Vascular (no infección)",
-      "Circulatory Disease" ~ "Cardiovascular",
-      "Infection Wound" ~ "Infección",
-      "Respiratory System Other Cause" ~ "Otras Causas",
-      "Infection COVID19 (SARS-CoV2 infection)" ~ "Infección",
-      "Access Placement" ~ "Acceso Vascular (no infección)",
-      "Blood Disorder Anemia" ~ "Otras Causas",
-      "Cerebrovascular Disease" ~ "Cardiovascular",
-      "Injury/Accident" ~ "Otras Causas",
-      "Cardiovascular Other" ~ "Cardiovascular",
-      "Blood Disorder Clotting" ~ "Otras Causas",
-      "Blood Disorder Other" ~ "Otras Causas",
-      "Endocrine Disorder Diabetes" ~ "Otras Causas",
-      "Cancer/Neoplasms" ~ "Otras Causas",
-      "Metabolic Disorder" ~ "Otras Causas",
-      "Respiratory System COPD" ~ "Otras Causas",
-      "Mental Disorder" ~ "Otras Causas",
-      "Infection Peritonitis (PD Related Peritonitis Excluded)" ~ "Infección",
-      "Skin Disorder" ~ "Otras Causas",
-      "Infection Tunnel/Cuff" ~ "Infección",
-      "PD Catheter Replacement" ~ "Otras Causas",
-      "Other PD Related Cause" ~ "Otras Causas",
-      "PD Catheter Placement" ~ "Otras Causas")),
-    hosp_cause = case_match(
-      hosp_cause,
-      "Infection Respiratory" ~ "Infección Respiratoria",
-      "Cardiovascular Pulmonary Disease" ~ "Enfermedad Pulmonar Cardiovascular",
-      "Digestive System Disorder" ~ "Trastorno del Sistema Digestivo",
-      "Other" ~ "Otras Causas",
-      "Access Repair" ~ "Reparación de Acceso Vascular",
-      "Musculoskeletal System Disorder" ~ "Trastorno del Sistema Musculoesquelético",
-      "Infection Blood" ~ "Bacteriemia",
-      "Cardiovascular Vessel Disease" ~ "Enfermedad Cardiovascular Periférica",
-      "Genitourinary Disorder" ~ "Trastorno Genitourinario",
-      "Cardiovascular Heart Disease" ~ "Enfermedad Cardiovascular",
-      "Access Infection" ~ "Infección de Acceso Vascular",
-      "Immune Disorder Other" ~ "Otros Trastornos Inmunológicos",
-      "Unknown" ~ "Desconocido",
-      "Infection Other" ~ "Otras Infecciones",
-      "Nervous System Disorder" ~ "Trastorno del Sistema Nervioso",
-      "Access Other" ~ "Otras Causas - Acceso Vascular",
-      "Circulatory Disease" ~ "Enfermedad Circulatoria",
-      "Infection Wound" ~ "Infección de Herida",
-      "Respiratory System Other Cause" ~ "Otras Causas Respiratorias",
-      "Infection COVID19 (SARS-CoV2 infection)" ~ "Infección COVID19 (Infección por SARS-CoV2)",
-      "Access Placement" ~ "Colocación de Acceso Vascular",
-      "Blood Disorder Anemia" ~ "Anemia",
-      "Cerebrovascular Disease" ~ "Enfermedad Cerebrovascular",
-      "Injury/Accident" ~ "Lesión/Accidente",
-      "Cardiovascular Other" ~ "Otros Trastornos Cardiovasculares",
-      "Blood Disorder Clotting" ~ "Trastorno de la Coagulación",
-      "Blood Disorder Other" ~ "Otros Trastornos Hematológicos",
-      "Endocrine Disorder Diabetes" ~ "Diabetes",
-      "Cancer/Neoplasms" ~ "Cáncer/Neoplasmas",
-      "Metabolic Disorder" ~ "Trastorno Metabólico",
-      "Respiratory System COPD" ~ "Enfermedad Pulmonar Obstructiva Crónica (EPOC)",
-      "Mental Disorder" ~ "Trastorno Mental",
-      "Infection Peritonitis (PD Related Peritonitis Excluded)" ~ "Peritonitis (Excluyendo Peritonitis Relacionada con PD)",
-      "Skin Disorder" ~ "Trastorno de la Piel",
-      "Infection Tunnel/Cuff" ~ "Infección de Túnel/Cuff",
-      "PD Catheter Replacement" ~ "Reemplazo de Catéter PD",
-      "Other PD Related Cause" ~ "Otras Causas Relacionadas con PD",
-      "PD Catheter Placement" ~ "Colocación de Catéter PD"))
-
-
-# Ánalisis de hospitalizaciones ####
 
 
